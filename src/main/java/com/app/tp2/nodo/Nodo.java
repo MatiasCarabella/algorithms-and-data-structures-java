99272e85b47578aa81d2aa08dbd855ffa8199c5b
Matias Carabella <matias.carabella@outlook.com>
2023-08-21 18:10:19 -0300

    Adding comments

--- src/main/java/com/app/tp2/nodo/Nodo.java
+++ src/main/java/com/app/tp2/nodo/Nodo.java
@@ -1,10 +1,16 @@
 package main.java.com.app.tp2.nodo;
 
 public class Nodo {
+    /**
+     * La clase Nodo posee 2 atributos:
+     * info - es un entero que almacena el valor en sí del nodo
+     * sig - una referencia al siguiente nodo en la lista enlazada
+     */
     public int info;
     public Nodo sig;
 
     public Nodo (int info) {
+        // El constructor de Nodo recibe el valor entero a almacenar en info
         this.info = info;
     }
 }
--- src/main/java/com/app/tp2/pila/Pila.java
+++ src/main/java/com/app/tp2/pila/Pila.java
@@ -4,29 +4,43 @@ import main.java.com.app.tp2.nodo.Nodo;
 
 public class Pila {
 
+    /**
+     * La clase Pila posee 1 atributo:
+     * raiz - una referencia al nodo raíz o 'tope' de la pila
+     */
     private Nodo raiz;
 
     public Pila() {
+        // El constructor de la pila inicializa la raíz en null
         this.raiz = null;
     }
 
     public void apilar(int x) {
+        // Para apilar un elemento, primero creamos un nodo con el valor deseado
         Nodo nuevo = new Nodo(x);
+        // Este nuevo nodo tendrá como siguiente nodo al anterior 'tope' de la pila o raíz
         nuevo.sig = raiz;
+        // Finalmente, el nuevo nodo pasa a ser el nuevo 'tope' de la pila o raíz
         this.raiz = nuevo;
     }
 
     public int desapilar() {
+        // Para desapilar un elemento, primero obtenemos el valor actual del 'tope' de la pila
         int info = raiz.info;
+        // Luego, movemos el 'tope' o raíz de la pila al elemento siguiente de la actual raíz
         this.raiz = raiz.sig;
+        // Finalmente, devolvemos el valor de la ex-raíz que acabamos de quitar de la pila
         return info;
     }
 
     public void verContenido() {
+        // Para mostrar el contenido de nuestra pila, partimos de la raiz y luego iteramos:
         Nodo nodo = this.raiz;
         while (nodo != null) {
+            // Mientras el nodo actual no sea null, mostraremos su valor y pasaremos al siguiente
             System.out.print(nodo.info);
             if (nodo.sig != null) {
+                // Para fines estéticos, mostramos una flecha '->' si el nodo siguiente no es nulo
                 System.out.print(" -> ");
             }
             nodo = nodo.sig;
